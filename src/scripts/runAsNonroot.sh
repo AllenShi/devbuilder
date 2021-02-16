e22a51222f0d3a97e8c608333f44ce795cefd7f0
AllenShi <jinliangshi@gmail.com>
2021-02-17 00:16:27 +0800

    optimize the environment var settings

--- src/scripts/runAsNonroot.sh
+++ src/scripts/runAsNonroot.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-# Step 5: ssh-keygen
+#!/bin/bash # Step 5: ssh-keygen
 printf "Step 5: ssh-keygen\n"
 printf "Current HOME is $HOME, current user is $(whoami)\n"
 [ ! -f $HOME/.ssh/id_rsa ] && ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -P ''
@@ -16,27 +14,31 @@ sudo yum install -y tmux
 
 # Step 8: install rustlang
 printf "Step 8: install rustlang\n"
+if [ command -v rustup &>/dev/null ] ; then 
+	rustup self uninstall
+fi
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rm -f rustup-init
 wget https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init && chmod +x rustup-init
 printf "1" | ./rustup-init 
-cat > $HOME/.cargo/config <<EOF
+cat > $HOME/.cargo/config <<-EOF
 [source.crates-io]
 registry = "https://github.com/rust-lang/crates.io-index"
 replace-with = 'ustc'
 [source.ustc]
-registry = "git://mirrors.ustc.edu.cn/crates.io-index"
+registry = "git://mirrors.ustc.edu.cn/crates.io-index" 
 EOF
-echo "export PATH=$HOME/.cargo/bin:$PATH" >> $HOME/.bash_profile
+grep -q ".cargo/bin" $HOME/.bash_profile || echo "export PATH=$HOME/.cargo/bin:$PATH" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 rustc --version
 
 # Step 9: install golang
 printf "Step 9: install golang\n"
-echo "export GOPROXY=https://mirrors.aliyun.com/goproxy/" >> $HOME/.bash_profile
+grep -q "GOPROXY" $HOME/.bash_profile || echo "export GOPROXY=https://mirrors.aliyun.com/goproxy/" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 # wget https://golang.org/dl/go1.15.8.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf ~/go1.15.8.linux-amd64.tar.gz
-echo "export PATH=/usr/local/go/bin:$PATH" >> $HOME/.bash_profile
+grep -q "/usr/local/go/bin" $HOME/.bash_profile || echo "export PATH=/usr/local/go/bin:$PATH" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 go version
 
